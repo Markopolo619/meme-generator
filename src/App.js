f23b79c47ee55601f0ee403b64f52b06a8f8302c
Mark <markmadi232@gmail.com>
2021-12-17 14:27:59 +0200

    Created React App and removed unnecessary code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Nav from './Components/Nav';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
